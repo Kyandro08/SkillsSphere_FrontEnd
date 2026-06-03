b80a4a344da9beb1732c84461543217a0e080351
jaspervkalsbeek-svg <jasper.v.kalsbeek@gmail.com>
2026-06-03 09:46:15 +0200

    Remove old Expo project, default starters, debug page

--- src/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import Constants from "expo-constants";
-
-// @ts-ignore - Constants.expoConfig.extra exists at runtime
-const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl;
-// @ts-ignore
-const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
